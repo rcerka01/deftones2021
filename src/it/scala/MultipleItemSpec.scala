3ec304e3bd2657a4211af645d3c5c226a3172e64
Ray <raitis.cerkasovs@gmail.com>
2021-08-25 11:46:05 +0100

    Create data source

--- src/it/scala/MultipleItemSpec.scala
+++ src/it/scala/MultipleItemSpec.scala
@@ -3,7 +3,7 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.Specs2RouteTest
 import deftones2021.api.Api
-import deftones2021.domain.Item
+import deftones2021.domain.Track
 import deftones2021.domain.response.{ErrorResponse, Response}
 import org.specs2.mutable.Specification
 
@@ -19,7 +19,7 @@ class MultipleItemSpec extends Specification
       Get("/items") ~> Route.seal(routes) ~> check {
         status must be equalTo StatusCodes.OK
         contentType must be equalTo `application/json`
-        responseAs[Response[Item]].total must be > 0
+        responseAs[Response[Track]].total must be > 0
         header("Cache-Control").get.value must be equalTo "public, max-age=300, s-maxage=300, stale-while-revalidate=7200, stale-if-error=7200"
       }
     }
@@ -27,7 +27,7 @@ class MultipleItemSpec extends Specification
     "return a response with the requested limit" in {
       Get("/items?limit=1") ~> Route.seal(routes) ~> check {
         status must be equalTo StatusCodes.OK
-        responseAs[Response[Item]].total must be equalTo 1
+        responseAs[Response[Track]].total must be equalTo 1
       }
     }
 
